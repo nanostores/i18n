8ca292c8b9ee3b208c726f92bd34c8f8ae415fd0
Alexey Berezin <beraliv.spb@gmail.com>
2022-02-07 19:44:23 +0100

    fix: 🧪 count

--- count/index.type.ts
+++ count/index.type.ts
@@ -3,16 +3,37 @@ import { AssertTrue as Assert, IsExact } from 'conditional-type-checks'
 import { count } from '.'
 // eslint-disable-next-line import/extensions
 import { TranslationFunction } from '../create-i18n'
+// eslint-disable-next-line import/extensions
+import { params } from '../params'
 
-type TestingTranslation = TranslationFunction<[number], string>
-
-const f = count({
+const f1 = count({
   one: 'One page',
   many: '{count} pages'
 })
 
+const f2 = count(
+  params<{ category: number }>({
+    one: 'One page in {category}',
+    many: '{count} pages in {category}'
+  })
+)
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type cases = [
-  Assert<IsExact<typeof f, TestingTranslation>>,
-  Assert<IsExact<ReturnType<typeof f>, string>>
+  Assert<IsExact<typeof f1, TranslationFunction<[number], string>>>,
+  Assert<IsExact<ReturnType<typeof f1>, string>>,
+  Assert<
+    IsExact<
+      typeof f2,
+      TranslationFunction<
+        [
+          {
+            category: number
+          }
+        ],
+        TranslationFunction<[number], string>
+      >
+    >
+  >,
+  Assert<IsExact<ReturnType<typeof f2>, TranslationFunction<[number], string>>>
 ]
