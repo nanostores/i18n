e04b6ba7c715a29127b20ab60bfa71ae67035812
Alexey Berezin <beraliv.spb@gmail.com>
2022-02-27 16:10:38 +0100

    test: 🧪 cover params & count types

--- count/types.ts
+++ count/types.ts
@@ -10,13 +10,14 @@ const f1 = count({
   one: 'One page',
   many: '{count} pages'
 })
-
 const f2 = count(
   params<{ category: number }>({
     one: 'One page in {category}',
     many: '{count} pages in {category}'
   })
 )
+const f21 = f2({ category: 12 })
+const f22 = f2(12)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type cases = [
@@ -25,15 +26,19 @@ type cases = [
   Assert<
     IsExact<
       typeof f2,
-      TranslationFunction<
-        [
-          {
-            category: number
-          }
-        ],
-        TranslationFunction<[number], string>
-      >
+      {
+        (input: { category: number }): TranslationFunction<[number], string>
+        (input: number): TranslationFunction<[{ category: number }], string>
+      }
     >
   >,
+  Assert<IsExact<typeof f21, TranslationFunction<[number], string>>>,
+  Assert<
+    IsExact<typeof f22, TranslationFunction<[{ category: number }], string>>
+  >,
+  // saving backward compatibility
+  // 1. Parameters
+  Assert<IsExact<Parameters<typeof f2>, [input: { category: number }]>>,
+  // 2. ReturnType
   Assert<IsExact<ReturnType<typeof f2>, TranslationFunction<[number], string>>>
 ]
--- params/types.ts
+++ params/types.ts
@@ -13,6 +13,8 @@ const f2 = params<{ category: number }>(
     many: '{count} pages in {category}'
   })
 )
+const f21 = f2({ category: 12 })
+const f22 = f2(12)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type cases = [
@@ -23,15 +25,19 @@ type cases = [
   Assert<
     IsExact<
       typeof f2,
-      TranslationFunction<
-        [
-          {
-            category: number
-          }
-        ],
-        TranslationFunction<[number], string>
-      >
+      {
+        (input: { category: number }): TranslationFunction<[number], string>
+        (input: number): TranslationFunction<[{ category: number }], string>
+      }
     >
   >,
+  Assert<IsExact<typeof f21, TranslationFunction<[number], string>>>,
+  Assert<
+    IsExact<typeof f22, TranslationFunction<[{ category: number }], string>>
+  >,
+  // saving backward compatibility
+  // 1. Parameters
+  Assert<IsExact<Parameters<typeof f2>, [input: { category: number }]>>,
+  // 2. ReturnType
   Assert<IsExact<ReturnType<typeof f2>, TranslationFunction<[number], string>>>
 ]
