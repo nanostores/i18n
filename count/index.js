ca329853e161ade86a5fee0f010260bdde892945
Andrey Sitnik <andrey@sitnik.es>
2026-04-15 13:22:41 +0000

    Clean up code

--- count/index.js
+++ count/index.js
@@ -2,7 +2,9 @@ import { strings, transform } from '../transforms/index.js'
 
 export const count = transform((locale, translation, num) => {
   let form = new Intl.PluralRules(locale).select(num)
-  if (!(form in translation)) form = 'other' in translation ? 'other' : 'many'
+  if (!(form in translation)) {
+    form = 'other' in translation ? 'other' : 'many'
+  }
 
   return strings(translation[form], str => str.replace(/{count}/g, num))
 })
